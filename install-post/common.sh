ba6b4ccd9e4f85dd6508f4b4d6cd2e4fc234c24b
Tony Ko <tony@htko.ca>
2020-10-01 05:33:27 +0000

    add script to Increase files watched

--- install-post/common.sh
+++ install-post/common.sh
@@ -7,3 +7,5 @@ source ../includes/sudo-check.sh
 echo "[install-post - common.sh]"
 echo 'source ~/.htkorc' | sudo tee -a ~/.bashrc
 echo 'source ~/.htkorc' | sudo tee -a ~/.zshrc
+echo 'fs.inotify.max_user_watches=32768' | sudo tee -a ~/.zshrc # increase files watched, using more ram
+sudo sysctl -p # apply the above file
