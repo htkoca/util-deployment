b4308b025cd435786056c72b1febd9ec521d75a3
Tony Ko <tony@htko.ca>
2020-10-01 05:03:47 +0000

    Install brew before git due to dependencies

--- setup-vm/install-env.sh
+++ setup-vm/install-env.sh
@@ -6,9 +6,9 @@ source ../includes/sudo-check.sh
 # scripts
 echo "[setup-vm - install-env.sh]"
 source ../install-env/common.sh
-source ../install-env/git.sh
-source ../install-env/docker.sh
 source ../install-env/homebrew.sh
+source ../install-env/docker.sh
+source ../install-env/git.sh
 source ../install-env/nvm.sh
 source ../install-env/ufw.sh
 source ../install-env/zsh.sh
--- setup-vm/install-post.sh
+++ setup-vm/install-post.sh
@@ -5,9 +5,9 @@ source ../includes/sudo-check.sh
 
 # scripts
 echo "[setup-vm - install-post.sh]"
-source ../install-post/git.sh
-source ../install-post/docker.sh
 source ../install-post/homebrew.sh
+source ../install-post/docker.sh
+source ../install-post/git.sh
 source ../install-post/nvm.sh
 source ../install-post/ufw.sh
 source ../install-post/zsh.sh
